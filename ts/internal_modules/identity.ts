0c218c267851ff2d0ab0c5f93efbee581298a136
charly <fei.charleen@gmail.com>
2018-01-22 15:38:45 +0100

    slight edits to variable naming for readability

--- ts/internal_modules/identity.ts
+++ ts/internal_modules/identity.ts
@@ -3,7 +3,8 @@ import * as bs58 from 'bs58'
 export default class DID {
   identifier: string
   constructor(publicKey: Buffer) {
-    const fragment = bs58.encode(publicKey)
-    this.identifier = 'did:jolo:'+ fragment
+    const prefix = 'did:jolo:'
+    const suffix = bs58.encode(publicKey)
+    this.identifier = prefix + suffix
   }
 }
