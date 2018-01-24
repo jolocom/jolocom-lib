9aad24d7738bd3d2fa129b1cccc1a8cc1ed61121
charly <fei.charleen@gmail.com>
2018-01-24 13:17:58 +0100

    set identifier as a public class member

---
+++ ts/identity/did.ts
@@ -0,0 +1,24 @@
+import * as bs58 from 'bs58'
+
+/* Creates Identity id according to DID/DDO specifications
+ * Source: https://w3c-ccg.github.io/did-spec/
+ */
+export default class Did{
+  public identifier: string
+  constructor(publicKey: Buffer) {
+    const prefix = 'did:jolo:'
+    const suffix = bs58.encode(publicKey.substr(0, 16))
+    this.identifier = prefix + suffix
+  }
+
+  toJSON(): string {
+    return this.identifier
+  }
+
+  static fromJson(id: string): Did {
+    let did = Object.create(Did.prototype)
+    return Object.assign(did, id, {
+      identifier: id
+    })
+  }
+}
