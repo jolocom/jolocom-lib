fedb2eb2919cf4300cbcac54c406e8f1d5bd3545
Kasia Szawan <kat.szwn@gmail.com>
2018-01-24 16:36:49 +0100

    Implement toJSON for Did

--- tests/data/identity.ts
+++ tests/data/identity.ts
@@ -1,13 +1,13 @@
 export default {
   testUserPublicKey: '04334f990cdc1281c6fed73dcc6de3db520c075a41ce2b93f36f85dbd5e0dc0b05975c51acb0bec9747fb3f52fd0146d1dc39d6077db0be1bd6579fd66ac144442',
-  testUserDID:'did:jolo:0xf334484858571199b681f6dfdd9ecd2f01df5b38f8379b3aaa89436c61fd1955',
+  testUserDID:'did:jolo:6xExKfgg2WRGBPLJeUhmYk',
   expectedDdoJson: '{' +
     '"@context":"https://w3id.org/did/v1",' +
-    '"id":"did:jolo:0xf334484858571199b681f6dfdd9ecd2f01df5b38f8379b3aaa89436c61fd1955",' +
+    '"id":"did:jolo:b2tpLeBvvdPPgiwQQpSe3",' +
     '"authenticationCredential":{' +
-      '"id":"did:jolo:0xf334484858571199b681f6dfdd9ecd2f01df5b38f8379b3aaa89436c61fd1955#keys/generic/1",' +
+      '"id":"did:jolo:b2tpLeBvvdPPgiwQQpSe3#keys/generic/1",' +
       '"type":["CryptographicKey","EcDsaSAKey"],' +
-      '"owner":"did:jolo:0xf334484858571199b681f6dfdd9ecd2f01df5b38f8379b3aaa89436c61fd1955",' +
+      '"owner":"did:jolo:b2tpLeBvvdPPgiwQQpSe3",' +
       '"curve":"secp256k1",' +
       '"publicKeyBase64":"04334f990cdc1281c6fed73dcc6de3db520c075a41ce2b93f36f85dbd5e0dc0b05975c51acb0bec9747fb3f52fd0146d1dc39d6077db0be1bd6579fd66ac144442"' +
     '},' +
