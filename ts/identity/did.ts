f5204953b455c3722e21f61381922b8531360876
Kasia Szawan <kat.szwn@gmail.com>
2018-01-24 14:11:06 +0100

    Make Did constructor take string instead of a Buffer

--- ts/identity/did.ts
+++ ts/identity/did.ts
@@ -5,9 +5,9 @@ import * as bs58 from 'bs58'
  */
 export default class Did{
   identifier: string
-  constructor(publicKey: Buffer) {
+  constructor(publicKey: string) {
     const prefix = 'did:jolo:'
-    const suffix = bs58.encode(publicKey.subarray(0, 16))
+    const suffix = bs58.encode(publicKey.substr(0, 16))
     this.identifier = prefix + suffix
   }
 }
