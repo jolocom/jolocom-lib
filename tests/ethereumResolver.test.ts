e0d4364b9b12d6741eba7a29fde033dfc7975e9b
Exulansis <rusu.eugeniu@gmx.de>
2018-01-29 15:39:28 +0100

    Basic test skeleton and empty class

---
+++ tests/ethereumResolver.test.ts
@@ -0,0 +1,23 @@
+import { expect } from 'chai'
+import testData from './data/identity'
+import EthereumResolver from '../ts/ethereum/ethereumResolver'
+
+describe('Ethereum Resolver', () => {
+  const ethResolver = new EthereumResolver()
+
+  it('Should correctly query contract for the user\'s DDO hash', () => {
+
+  })
+
+  it('Should return error in case reading record fails', () => {
+  
+  })
+
+  it('Should correctly register a user\'s DDO hash', () => {
+
+  })
+
+  it('Should return error in case writting record fails', () => {
+  
+  })
+})
