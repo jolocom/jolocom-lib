f9f7e07bbb3e18e69d9dd421dd4dbea299d5bd67
Exulansis <rusu.eugeniu@gmx.de>
2018-01-29 21:14:41 +0100

    Tests for ethereumResolver and working implementation

--- tests/ethereumResolver.test.ts
+++ tests/ethereumResolver.test.ts
@@ -1,23 +1,43 @@
-import { expect } from 'chai'
-import testData from './data/identity'
+import * as chai from 'chai'
+import * as chaiAsPromised from 'chai-as-promised'
+
+import identityData from './data/identity'
+import ethereumData from './data/ethereum'
+
 import EthereumResolver from '../ts/ethereum/ethereumResolver'
+
+chai.use(chaiAsPromised)
+const expect = chai.expect
+
 
 describe('Ethereum Resolver', () => {
   const ethResolver = new EthereumResolver()
+  it('Should correctly register a user\'s DDO hash', async () => {
+    const hash = await ethResolver.updateDIDRecord(
+      ethereumData.firstTestAccount, identityData.testUserDID,
+      ethereumData.mockDDOHash
+    )
 
-  it('Should correctly query contract for the user\'s DDO hash', () => {
-
+    expect(await ethResolver.resolveDID(identityData.testUserDID))
+      .to.equal(ethereumData.mockDDOHash)
   })
 
-  it('Should return error in case reading record fails', () => {
-  
+  it('Should return error in case writting record fails', async () => {
+    await expect(ethResolver.updateDIDRecord(
+      ethereumData.secondTestAcount,
+      identityData.testUserDID,
+      ethereumData.mockDDOHash
+    )).to.be.rejectedWith(
+      'Returned error: VM Exception while processing transaction: revert'
+    )
   })
 
-  it('Should correctly register a user\'s DDO hash', () => {
-
+  it('Should correctly query contract for the user\'s DDO hash', async () => {
+    const hash = await ethResolver.resolveDID(identityData.testUserDID)
+    expect(hash).to.equal(ethereumData.mockDDOHash)
   })
 
-  it('Should return error in case writting record fails', () => {
-  
+  it('Should return error in case reading record fails', async () => {
+    await expect(ethResolver.resolveDID('invalidInput')).to.be.rejected
   })
 })
--- ts/ethereum/ethereumResolver.ts
+++ ts/ethereum/ethereumResolver.ts
@@ -1,4 +1,54 @@
+import * as Web3 from 'web3'
+import * as RegistryContract from '../../contracts/Registry.json'
+
 export default class EthereumResolver{
   constructor() {
+    const address = '0xc4b48901af7891d83ce83877e1f8fb4c81a94907'
+
+    this.web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'))
+    this.indexContract = new this.web3.eth.Contract(RegistryContract.abi, address)
+  }
+  
+  resolveDID(did: string): Promise<string> {
+    return new Promise((resolve, reject) => {
+      const keyHash = this._stripMethodPrefix(did)
+      this.indexContract.methods.getRecord(keyHash).call((error, result) => {
+        if (error) {
+          return reject(error)
+        }
+        return resolve(result)
+      })
+    })
+  }
+
+  updateDIDRecord(sender: string, did: string, newHash: string): Promise<string> {
+    return new Promise((resolve, reject) => {
+      const keyHash = this._stripMethodPrefix(did)
+      
+
+      this.indexContract.methods.setRecord(keyHash, newHash).send({
+        from: sender,
+      },(error, result) => {
+        if (error) {
+          return reject(error)
+        }
+
+        /* The web3 provider needs to support WSS, 
+         * will be configured at later stage
+
+        this.indexContract.events.registrationSuccess((error, result) => {
+          console.log(error)
+          console.log(result)
+        })
+
+        */
+
+        return resolve()
+      })
+    })
+  }
+
+  private _stripMethodPrefix(did: string): string {
+    return did.substring(did.lastIndexOf(':') + 1)
   }
 }
