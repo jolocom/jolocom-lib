7fbf07b267d6b56e5ec61a021a3f716117e3822c
nataschaberg <natalja.wittenberg@gmx.de>
2018-01-31 17:15:46 +0100

    small refactor

--- ts/authentication.ts
+++ ts/authentication.ts
@@ -6,13 +6,14 @@ import * as QRCode from 'qrcode'
 import { TokenSigner, TokenVerifier, decodeToken } from 'jsontokens'
 import * as bitcoin from 'bitcoinjs-lib'
 
-export async function createQRCode(did : String, claims : Array<any>, IPFSroom : String) : Promise<any> {
+export async function createQRCode({did, claims, IPFSroom, WIF} :
+  {did : string, claims : Array<string>, IPFSroom : string, WIF : string}) : Promise<any> {
 
-  const keys = _getSigningKeysFromWIF()
+  const keys = _getSigningKeysFromWIF({WIF: WIF})
   const tokenPayload = new TokenPayload({
-    iss: 'did:jolo:6xExKfgg2WRGBPLJeUhmYk',
-    reqClaims: [ 'name', 'proofOfAge' ],
-    IPFSroom: 'fekrnkegr',
+    iss: did,
+    reqClaims: claims,
+    IPFSroom: IPFSroom,
     pubKeyIss: keys.pubKey
   })
   const token = new TokenSigner('ES256k', keys.privKey).sign(tokenPayload)
@@ -25,30 +26,31 @@ export async function createQRCode(did : String, claims : Array<any>, IPFSroom :
   })
 }
 
-// createQRCode(testAuth.mockDIDISS, [ 'name', 'proofOfAge' ], 'kernjreng')
-
-
-export async function authenticateRequest(token : String) {
+export async function authenticateRequest({token, WIF} :
+  {token : string, WIF : string}) {
   const tokenData = decodeToken(token)
   const verify = new TokenVerifier('ES256k', tokenData.payload.pubKeyIss).verify(token)
 
   if(verify) {
-    /* TODO: get claims for requested fields for private claims
-    get did for public claims; 2nd step: check one verification from requester
+    /* TODO:
+      1. get did for user persona with public claims to add it to communication
+      2. resolve did iss to ddo of requester
+      3. form ddo, get one claim hash and resolve to claim information
     */
 
-    // const keys = _getSigningKeysFromWIF()
+    const did = _getDID()
+    const keys = _getSigningKeysFromWIF({WIF: WIF})
 
     const tokenPayload = TokenPayload.generateResponse({
       tokenData: tokenData,
-      sub: testAuth.mockDIDSUB,
-      pubKeySub: testAuth.rawPublicKey,
-      claims: {name: 'Natascha'}
+      sub: did, // mock
+      pubKeySub: keys.pubKey,
+      claims: {name: 'Natascha'} // mock
     })
 
     const token = new TokenSigner('ES256K', testAuth.rawPrivateKey).sign(tokenPayload)
 
-    // TODO: post to IPFS random room here
+    // TODO: post token to IPFS random room
 
     return 'OK'
   } else {
@@ -58,24 +60,25 @@ export async function authenticateRequest(token : String) {
 
 
 
-export async function authenticateResponse(token : String) {
+export async function authenticateResponse({token} : {token : string}) {
   const tokenData = decodeToken(token)
   const verify = new TokenVerifier('ES256k', tokenData.payload.pubKeySub).verify(token)
   if(verify) {
-    /*TODO: resolve did to ddo; resolve claims in service endpoints section and check respective issuer
+    /*TODO:
+      1. resolve did sub to ddo
+      2. resolve claims in service endpoints section and check respective issuer
+      3. some interaction should happen here on trusting the issuer of verification from claim
     */
     const mockDDO = testAuth.ddo
+
+    // TODO: post token to IPFS random room here
     return 'OK'
   } else {
     return new Error('Web Token Not Valid')
   }
 }
 
-// authenticateRequest(testAuth.mockTokenReq)
-// authenticateResponse(testAuth.mockTokenRes)
-
-function _getSigningKeysFromWIF() {
-  const WIF = 'L1Xs8xNygctCDgry2UsYCPywgC1WUckEePZ9NGdZswTzhjoAooNu'
+function _getSigningKeysFromWIF({WIF} : {WIF : string}) {
   const keyPair = bitcoin.ECPair.fromWIF(WIF)
 
   let keys = {
@@ -84,4 +87,8 @@ function _getSigningKeysFromWIF() {
   }
 
   return keys
+}
+
+function _getDID() {
+  return 'did:jolo:f3ern3prgkpernn' // mock
 }
