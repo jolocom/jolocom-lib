80c96e43ca84c742c34760138803d61a39bc1614
nataschaberg <natalja.wittenberg@gmx.de>
2018-02-14 16:14:43 +0100

    expiry date implemented for claims

--- tests/verifiableCredential.test.ts
+++ tests/verifiableCredential.test.ts
@@ -6,24 +6,35 @@ import testDataKeys from './data/keys'
 import * as lolex from 'lolex'
 import * as sinon from 'sinon'
 import * as bitcoin from 'bitcoinjs-lib'
+import * as moment from 'moment'
 
-describe('VerifiableCredential', () => {
+describe.only('VerifiableCredential', () => {
   const did = new Did(testDataIdentity.testUserPublicKey)
-  const clock = lolex.install({now: new Date('2018-01-24T15:42:15.882Z')})
+  const unixEpoch = moment.utc('2018-01-24T15:42:15Z', moment.ISO_8601).valueOf()
+  const clock = lolex.install({now: unixEpoch})
   sinon.stub(VerifiableCredential.prototype, 'generateVerifiableCredentialID').returns('11111')
 
   it('Should correctly instantiate a Verifiable Credential', () => {
     const credentialType = ['Credential']
     const issuerID ='did:jolo:0x0aecaae09eb4b6433a4136fbac29a5ea93dd3593dd120e16a314744d3945d119'
     const claim ={id:'did:jolo:0xd0ae58da9f72c48767b04f339a1a0142bb8e86b521d008ca65f7e3983b03d32b', ageOver:21}
-    const vc = new VerifiableCredential(credentialType, issuerID, claim)
+    const vc = new VerifiableCredential({
+      credentialType,
+      issuer: issuerID,
+      claim
+    })
     expect(JSON.stringify(vc)).to.equal(JSON.stringify(testDataIdentity.expectedVerifiedCredential))
   })
 
   it('#create correctly generates and returns a signed verifiable credential' , () => {
     const credentialType = ['Credential']
     const claim ={id:'did:jolo:0xd0ae58da9f72c48767b04f339a1a0142bb8e86b521d008ca65f7e3983b03d32b', ageOver: 21}
-    const result = VerifiableCredential.createVerified(did.identifier, credentialType, claim, testDataKeys.testGenericKeyPairWIF)
+    const result = VerifiableCredential.createVerified({
+      issuer: did.identifier,
+      credentialType,
+      claim,
+      privateKeyWIF: testDataKeys.testGenericKeyPairWIF
+    })
     expect(JSON.stringify(result)).to.equal(JSON.stringify(testDataIdentity.expectedSignedCredential))
   })
 
@@ -33,8 +44,46 @@ describe('VerifiableCredential', () => {
 
     const credentialType = ['Credential']
     const claim ={id:'did:jolo:0xd0ae58da9f72c48767b04f339a1a0142bb8e86b521d008ca65f7e3983b03d32b', ageOver:21}
-    const signed = VerifiableCredential.createVerified(did.identifier, credentialType, claim, testDataKeys.testGenericKeyPairWIF)
-    const result = signed.credential.verifySignedCredential(signed.signature, genericPublicKey)
+    const signed = VerifiableCredential.createVerified({
+      issuer: did.identifier,
+      credentialType,
+      claim,
+      privateKeyWIF: testDataKeys.testGenericKeyPairWIF
+    })
+    const result = signed.credential.verifySignedCredential({
+      signature: signed.signature,
+      publicKeyOfIssuer: genericPublicKey
+    })
     expect(result).to.equal(true)
+  })
+
+
+  it('#create throws an error in case of invalid expiry date' , () => {
+    const credentialType = ['Credential']
+    const claim ={id:'did:jolo:0xd0ae58da9f72c48767b04f339a1a0142bb8e86b521d008ca65f7e3983b03d32b', ageOver: 21}
+    const expires = moment.utc().format()
+
+    expect(VerifiableCredential.createVerified.bind(VerifiableCredential, {
+      issuer: did.identifier,
+      credentialType,
+      claim,
+      privateKeyWIF: testDataKeys.testGenericKeyPairWIF,
+      expires: expires
+    })).to.throw('The provided expiry date of 2018-01-24T15:42:15Z is not valid or the format you pass in is not compliant to ISO_8601.')
+  })
+
+  it('#create correctly generates and returns a signed verifiable credential with an expiry date' , () => {
+    const credentialType = ['Credential']
+    const claim ={id:'did:jolo:0xd0ae58da9f72c48767b04f339a1a0142bb8e86b521d008ca65f7e3983b03d32b', ageOver: 21}
+    const expires = moment.utc('2018-10-24T15:42:15Z', moment.ISO_8601).format()
+    const result = VerifiableCredential.createVerified({
+      issuer: did.identifier,
+      credentialType,
+      claim,
+      privateKeyWIF: testDataKeys.testGenericKeyPairWIF,
+      expires: expires
+    })
+
+    expect(JSON.stringify(result)).to.equal(JSON.stringify(testDataIdentity.expectedSignedCredentialWithExpiry))
   })
 })
--- ts/claims/verifiableCredential.ts
+++ ts/claims/verifiableCredential.ts
@@ -5,6 +5,7 @@ const Web3 = require('web3')
 import { IClaim } from './types'
 import { IVerifiedCredential } from './types'
 import { sign, verify } from '../utils/signature'
+import * as moment from 'moment'
 
 export default class VerifiableCredential implements IVerifiableCredentialAttrs {
   public '@context': string = "https://w3id.org/credentials/v1"
@@ -13,27 +14,51 @@ export default class VerifiableCredential implements IVerifiableCredentialAttrs 
   public issuer: string
   public issued: string
   public claim: IClaim
+  public expires: string
 
-  constructor(credentialType: string[], issuerID: string, claim: IClaim) {
+  constructor({credentialType, issuer, claim, expires } :
+    {credentialType: string[], issuer: string, claim: IClaim, expires?: string}) {
     this.id =  (this as any).generateVerifiableCredentialID()
     this.type = credentialType
-    this.issuer = issuerID
+    this.issuer = issuer
     this.issued = (this as any).dateIssued()
     this.claim = claim
+    this.expires = expires
   }
 
-  public static createVerified(issuer: string, credentialType: string[], claim: IClaim, privateKeyWIF: string) :
+  public static createVerified({issuer, credentialType, claim, privateKeyWIF, expires} :
+    {issuer: string, credentialType: string[], claim: IClaim, privateKeyWIF: string, expires?: string}) :
   IVerifiedCredential {
-    const unsignedCredential = new VerifiableCredential(credentialType, issuer, claim)
-    return unsignedCredential.signCredential(privateKeyWIF)
+    if (expires) {
+      const now = moment.utc()
+      const expiry = moment.utc(expires, moment.ISO_8601)
+      const validFormat = expiry.isValid()
+      const validExpiryDate = moment.duration(expiry.diff(now)).asHours()
+
+      if (validExpiryDate > 1 && validFormat) {
+        const unsignedCredential = new VerifiableCredential({credentialType, issuer, claim, expires})
+        return unsignedCredential.signCredential({privateKeyWIF})
+      } else {
+        throw new Error(`The provided expiry date of ${expires} is not valid or the format you pass in is not compliant to ISO_8601.`)
+      }
+    }
+
+    const unsignedCredential = new VerifiableCredential({
+      credentialType,
+      issuer,
+      claim,
+      expires
+    })
+    return unsignedCredential.signCredential({privateKeyWIF})
   }
 
-  public verifySignedCredential(signature: string, publicKeyOfIssuer: string) : boolean {
+  public verifySignedCredential({signature, publicKeyOfIssuer} :
+    {signature: string, publicKeyOfIssuer: string}) : boolean {
     const buffer = Buffer.from(signature, 'hex')
     return verify(this, publicKeyOfIssuer, buffer)
   }
 
-  private signCredential(privateKeyWIF: string) : IVerifiedCredential {
+  private signCredential({privateKeyWIF} : {privateKeyWIF: string}) : IVerifiedCredential {
     const keyPair = bitcoin.ECPair.fromWIF(privateKeyWIF)
     const privateKey = keyPair.d.toBuffer(32)
     const signature = sign(this, privateKey, keyPair.compressed)
@@ -52,6 +77,6 @@ export default class VerifiableCredential implements IVerifiableCredentialAttrs 
   }
 
   private dateIssued() : string {
-    return new Date(Date.now()).toString()
+    return moment.utc().format()
   }
 }
