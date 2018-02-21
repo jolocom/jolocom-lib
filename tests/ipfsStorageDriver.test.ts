88ef18d0752cd40c0fae799d4bc20293bacb0402
charly <fei.charleen@gmail.com>
2018-02-21 15:06:44 +0100

    removed only flag to allow all tests

--- tests/ipfsStorageDriver.test.ts
+++ tests/ipfsStorageDriver.test.ts
@@ -9,7 +9,7 @@ import data from '../tests/data/identity'
 chai.use(chaiAsPromised)
 const expect = chai.expect
 
-describe.only('IpfsStorageAgent', () => {
+describe('IpfsStorageAgent', () => {
   let StorageAgent
   const testData = {value: 'test'}
   const testDataHash = 'QmTEST'
