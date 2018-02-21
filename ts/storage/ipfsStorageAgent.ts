185427c50b8101d7160a445afe3694230397b9ec
charly <fei.charleen@gmail.com>
2018-02-21 11:51:35 +0100

    WIP: tests for IPLD storage of verifiable claims

--- ts/storage/ipfsStorageAgent.ts
+++ ts/storage/ipfsStorageAgent.ts
@@ -42,7 +42,7 @@ export default class IpfsStorageAgent {
     })
   }
 
-  createCredentialObject({credential: credential, dagLinks: dagLinks = []}) : Promise<object> {
+  createCredentialObject({ credential, dagLinks = [] } : { credential: Buffer, dagLinks: any }) : Promise<object> {
     return new Promise((resolve, reject) => {
       return this.ipfs.object.put(credential, dagLinks, (err,node) => {
         if (err) {
@@ -53,19 +53,19 @@ export default class IpfsStorageAgent {
     })
   }
 
-  getCredentialObject({multihash: multihash, getData: enabled}) : Promise<object> {
+  getCredentialObject({ multihash, getData } : { multihash: string, getData: boolean}) : Promise<object> {
     return new Promise((resolve, reject) => {
       return this.ipfs.object.get(multihash, (err, node) => {
         if (err) {
           return reject (err)
         }
-        return enabled ? resolve (JSON.parse(node.toJSON().data.toString())) : resolve(node)
+        return getData ? resolve (JSON.parse(node.toJSON().data.toString())) : resolve(node)
       })
     })
   }
 
   //update DDO with link information
-  addLink({ headNode: headNode, claimID: claimID, linkNode: linkNode }) : Promise<object> {
+  addLink({ headNode, claimID, linkNode } : { headNode : any,  claimID : string, linkNode : any }) : Promise<object> {
     return new Promise((resolve, reject) => {
       const linkNodeSize = linkNode.toJSON().size
       const linkNodeMultihash = linkNode.toJSON().multihash
@@ -79,9 +79,10 @@ export default class IpfsStorageAgent {
     })
   }
 
- resolveLinkPath({headNodeMultihash: headNodeMultihash, claimID: claimID}) : Promise<object> {
+  resolveLinkPath({ headNodeMultihash, claimID } : { headNodeMultihash: string, claimID: string }) : Promise<object> {
    return new Promise( async (resolve, reject) => {
       const modifiedHeadNode = await this.getCredentialObject({multihash: headNodeMultihash, getData: false})
+      console.log('here')
       const linkPath = 'Links/'+ claimID
       return dagPB.resolver.resolve(modifiedHeadNode.serialized, linkPath, async (error, result) => {
         const data = await this.getCredentialObject({multihash: result.value, getData: true})
@@ -90,41 +91,3 @@ export default class IpfsStorageAgent {
     })
   }
 }
-
-const config = {
-  host: 'localhost',
-  port: 5001,
-  protocol: 'http'
-}
-
-const data0 = {
-  credential: new Buffer('some identity stuff'),
-  dagLinks:[]
-}
-const data1 = {
-  credential: new Buffer(JSON.stringify(testData.expectedSignedCredential)),
-  dagLinks: []
-}
-
-const data2 = {
-  credential: new Buffer(JSON.stringify(testData.expectedVerifiedCredential)),
-  dagLinks: []
-}
-
-const ipfs = new IpfsStorageAgent(config)
-
-const wrapper = async () => {
-
-  const node1 = await ipfs.createCredentialObject(data0)
-  const node2 = await ipfs.createCredentialObject(data2)
-  const node3 = await ipfs.createCredentialObject(data1)
-
-  const modifiedHead = await ipfs.addLink({headNode: node1, claimID:'claimID', linkNode: node2})
-  const modifiedHeadNode = await ipfs.addLink({headNode: modifiedHead, claimID: 'claimID2', linkNode: node3})
-  const hash = modifiedHeadNode.toJSON().multihash
-  const head = await ipfs.getCredentialObject({multihash: hash, getData: false})
-  const data = await ipfs.resolveLinkPath({headNodeMultihash: head.toJSON().multihash, claimID: 'claimID2'})
-  console.log(data, 'data')
-}
-
-wrapper()
