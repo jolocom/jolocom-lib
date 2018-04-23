02952dfd122ae7cda8bbe5d59cf6d9fb0e94cf44
Exulansis <rusu.eugeniu@gmx.de>
2018-04-23 17:53:57 +0200

    Using custom ipfs library

--- ts/storage/ipfsStorageAgent.ts
+++ ts/storage/ipfsStorageAgent.ts
@@ -1,31 +1,19 @@
-import * as ipfsAPI from 'ipfs-api'
+import IpfsWrapper from 'ipfs-wrapper'
 
 export default class IpfsStorageAgent {
   ipfs: any
 
-  constructor(config : {
-    host: string,
-    port: number,
-    protocol: string
-  }) {
-    this.ipfs = new ipfsAPI(config)
+  constructor(config : { host: string, }) {
+    this.ipfs = new IpfsWrapper(config)
   }
 
-
-  async storeJSON(data : object) : Promise<object> {
+  async storeJSON(data : object) : Promise<string> {
     if (typeof data !== 'object' || data === null) {
       throw new Error(`JSON expected, received ${typeof data}`)
     }
 
-    const buffData = Buffer.from(JSON.stringify(data))
-
-    try {
-      const fileHash = await this.ipfs.files.add(buffData)
-      const pinned = await this.pinHash(fileHash[0].hash)
-      return pinned[0].hash
-    } catch(err) {
-      throw new Error(err)
-    }
+    const hash = await this.ipfs.add(JSON.stringify(data))
+    return hash
   }
 
   async catJSON(hash: string) : Promise<object> {
