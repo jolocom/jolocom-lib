6f24442c82580f4a50f128c13cafdb608e8b0623
Kasia Szawan <kat.szwn@gmail.com>
2018-06-14 16:11:13 +0200

    Implement authenticate method, that return fully fledged IdentityWallet

--- ts/identityWallet/identityWallet.ts
+++ ts/identityWallet/identityWallet.ts
@@ -2,6 +2,7 @@ import { DidDocument } from '../identity/didDocument';
 
 export class IdentityWallet {
   private didDocument: DidDocument
+  private privateIdentityKey: Buffer
 
   public getDidDocument(): DidDocument {
     return this.didDocument
@@ -9,5 +10,13 @@ export class IdentityWallet {
 
   public setDidDocument({didDocument}: {didDocument: DidDocument}): void {
     this.didDocument = didDocument
+  }
+
+  public getPrivateIdentityKey(): Buffer {
+    return this.privateIdentityKey
+  }
+
+  public setPrivateIdentityKey({privateIdentityKey: Buffer}): void {
+    this.privateIdentityKey = this.privateIdentityKey
   }
 }
--- ts/registries/jolocom.ts
+++ ts/registries/jolocom.ts
@@ -1,8 +1,9 @@
 import { IIpfsConnector } from '../ipfs/types';
 import { IEthereumConnector } from '../ethereum/types';
 import { IdentityWallet } from '../identityWallet/identityWallet';
-import { privateKeyToPublicKey } from '../utils/crypto'
+import { privateKeyToPublicKey, privateKeyToDID } from '../utils/crypto'
 import { DidDocument } from '../identity/didDocument'
+import { IDidDocumentAttrs } from '../identity/didDocument/types';
 
 /** Jolocom specific Registry, which uses IPFS
  *  and Ethereum for registering the indentity and the resolution
@@ -39,7 +40,7 @@ class Jolocom {
     {wallet, privateEthereumKey}: {wallet: IdentityWallet, privateEthereumKey: Buffer}
   ): Promise<void> {
     const ddo = wallet.getDidDocument()
-    let ipfsHash = ''
+    let ipfsHash
     try {
       ipfsHash = await this.ipfsConnector.storeJSON({data: ddo, pin: true})
     } catch (error) {
@@ -50,8 +51,20 @@ class Jolocom {
       ).catch((error) => { throw new Error(`Could not register DID record on Ethereum. ${error.message}`) })
   }
 
-  public async resolve({did}: {did: string}): Promise<string> {
-     return this.ethereumConnector.resolveDID({did})
-      .catch((error) => { throw new Error(`Could not retrieve DID Document. ${error.message}`) })
+  public async resolve({did}: {did: string}): Promise<IDidDocumentAttrs> {
+    // TODO: return an instance of Identity (which is extended DDO)
+    return this.ipfsConnector.catJSON({hash: await this.ethereumConnector.resolveDID({did})})
+     .catch((error) => {
+        throw new Error(`Could not retrieve DID Document. ${error.message}`)
+      }) as Promise<IDidDocumentAttrs>
+  }
+
+  public async authenticate({privateIdentityKey}: {privateIdentityKey: Buffer}): IdentityWallet {
+    const identityWallet = new IdentityWallet()
+    const did = privateKeyToDID(privateIdentityKey)
+    // TODO: change according to return of resolve
+    const ddoAttrs = await this.resolve({did})
+    const didDocument = DidDocument.fromJSON(ddoAttrs)
+    identityWallet.setDidDocument({didDocument})
   }
 }
