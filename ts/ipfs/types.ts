b71ec107567ffa8ef7e89e9bfe63088a5018d3a2
charly <ch4rly@riseup.net>
2018-06-14 15:01:23 +0200

    IPFS object put and get methods

--- ts/ipfs/types.ts
+++ ts/ipfs/types.ts
@@ -10,11 +10,5 @@ export interface IIpfsConnector {
   catJSON: (hash: string) => Promise<object>
   removePinnedHash: (hash: string) => Promise<void>
   createDagObject: (data: string, pin: boolean ) => Promise<string>
-  getDagObject: (hash: string) => Promise<object>
-  getDagObjectData: (hash: string) => Promise<string>
-  getDagObjectLinks: (hash: string) => Promise<string>
-  getDagObjectStat: (hash: string) => Promise<object>
-  // tslint:disable-next-line:max-line-length
-  addDagLink: ({ headNodeHash, claimId, linkNodeHash }: { headNodeHash: string, claimId: string, linkNodeHash: string } ) => Promise<object>
-  resolveDagLink: ({ headNodeHash, claimId }: { headNodeHash: string, claimId: string }) => Promise<object>
+  getDagObjectData: (hash: string, getData: boolean) => Promise<string>
 }
