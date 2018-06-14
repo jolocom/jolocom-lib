98d22254f4c523040cc8a3ae4748de3edfd54648
charly <ch4rly@riseup.net>
2018-06-14 15:01:23 +0200

    IPFS object put and get methods

--- ts/ipfs/index.ts
+++ ts/ipfs/index.ts
@@ -93,49 +93,12 @@ export class IpfsStorageAgent implements IIpfsConnector {
     return res.Hash
   }
 
-  public async getDagObject(hash: string ): Promise<object> {
+  public async getDagObjectData(hash: string, getData: boolean): Promise<string> {
+    console.log(hash, 'hash')
     const endpoint = `${this.endpoint}/api/v0/object/get?arg=${hash}`
     const res = await fetch(endpoint)
+
     return res.json()
   }
 
-  public async getDagObjectData(hash: string ): Promise<string> {
-    const endpoint = `${this.endpoint}/api/v0/object/data?arg=${hash}`
-    const res = await fetch(endpoint)
-    return res.text()
-  }
-
-  public async getDagObjectStat(hash: string ): Promise<object> {
-    const endpoint = `${this.endpoint}/api/v0/object/stat?arg=${hash}`
-    const res = await fetch(endpoint)
-    return res.json()
-  }
-
-  public async getDagObjectLinks(hash: string ): Promise<string> {
-    const endpoint = `${this.endpoint}/api/v0/object/links?arg=${hash}`
-    const res = await fetch(endpoint)
-    const fullRes = res.json()
-    if ( !fullRes.Links ) {
-      return 'No available links on this DAG object.'
-    } else {
-      return fullRes.Links
-    }
-  }
-
-  public async addDagLink({
-    headNodeHash, claimId, linkNodeHash
-  }: {
-    headNodeHash: string, claimId: string, linkNodeHash: string
-  }): Promise<object> {
-    // tslint:disable-next-line:max-line-length
-    const endpoint = `${this.endpoint}/api/v0/object/patch/add-link?arg=${headNodeHash}&arg=${claimId}&arg=${linkNodeHash}`
-    const newHeadNode = await fetch(endpoint)
-    return newHeadNode.json()
-  }
-
-  public async resolveDagLink({ headNodeHash, claimId }: { headNodeHash: string, claimId: string }): Promise<object> {
-    const endpoint = `${this.endpoint}/api/v0/object/get?arg=${headNodeHash}/${claimId}`
-    const resolvedNode = await fetch(endpoint)
-    return resolvedNode.json()
-  }
 }
