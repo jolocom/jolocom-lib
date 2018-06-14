4fd708aef7e5c78884f2a027aa0a69c2bce957c5
charly <ch4rly@riseup.net>
2018-06-14 15:01:23 +0200

    IPFS object put and get methods

--- ts/ipfs/types.ts
+++ ts/ipfs/types.ts
@@ -9,6 +9,11 @@ export interface IIpfsConnector {
   storeJSON: (data: object, pin: boolean) => Promise<string>
   catJSON: (hash: string) => Promise<object>
   removePinnedHash: (hash: string) => Promise<void>
+<<<<<<< HEAD
   createDagObject: (data: object, pin: boolean ) => Promise<string>
   resolveIpldPath: (pathToResolve: string) => Promise<object>
+=======
+  createDagObject: (data: string, pin: boolean ) => Promise<string>
+  getDagObjectData: (hash: string, getData: boolean) => Promise<string>
+>>>>>>> IPFS object put and get methods
 }
