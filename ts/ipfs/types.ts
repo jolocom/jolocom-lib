9c89c84c13d32683e6a435a1cf36531ba62c0d75
charly <ch4rly@riseup.net>
2018-06-15 13:47:39 +0200

    add and resolve DAG links

--- ts/ipfs/types.ts
+++ ts/ipfs/types.ts
@@ -9,6 +9,12 @@ export interface IIpfsConnector {
   storeJSON: (data: object, pin: boolean) => Promise<string>
   catJSON: (hash: string) => Promise<object>
   removePinnedHash: (hash: string) => Promise<void>
-  createDagObject: (data: object, pin: boolean ) => Promise<string>
-  resolveIpldPath: (pathToResolve: string) => Promise<object>
+  createDagObject: (data: string, pin: boolean ) => Promise<string>
+  getDagObject: (hash: string) => Promise<object>
+  getDagObjectData: (hash: string) => Promise<string>
+  getDagObjectLinks: (hash: string) => Promise<string>
+  getDagObjectStat: (hash: string) => Promise<object>
+  // tslint:disable-next-line:max-line-length
+  addDagLink: ({ headNodeHash, claimId, linkNodeHash }: { headNodeHash: string, claimId: string, linkNodeHash: string } ) => Promise<object>
+  resolveDagLink: ({ headNodeHash, claimId }: { headNodeHash: string, claimId: string }) => Promise<object>
 }
