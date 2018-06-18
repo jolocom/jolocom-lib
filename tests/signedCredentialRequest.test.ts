1a1aa03d5881bff386c068507acd96745e573831
Exulansis <rusu.eugeniu@gmx.de>
2018-06-18 12:48:51 +0200

    Defined test cases for signedCredentialRequest

---
+++ tests/signedCredentialRequest.test.ts
@@ -0,0 +1,25 @@
+import { expect } from 'chai'
+
+describe('SignedCredentialRequest', () => {
+  it('Should implement static fromJSON method', () => {
+    expect(false).to.equal(true)
+  })
+  it('Should implement toJSON method', () => {
+    expect(false).to.equal(true)
+  })
+  it('Should implement static fromJWT method', () => {
+    expect(false).to.equal(true)
+  })
+  it('Should implement toJWT method', () => {
+    expect(false).to.equal(true)
+  })
+  it('Should implement all getter methods', () => {
+    expect(false).to.equal(true)
+  })
+  it('Should implement a validateSignature method', () => {
+    expect(false).to.equal(true)
+  })
+  it('Should implement an applyConstraints method', () => {
+    expect(false).to.equal(true)
+  })
+})
