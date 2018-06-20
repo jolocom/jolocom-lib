7ddac36009bd6951e930d5d476eec806dc02cf0c
charly <ch4rly@riseup.net>
2018-06-20 17:40:25 +0200

    added publicKey section to DidDocument interface

--- ts/identity/didDocument/types.ts
+++ ts/identity/didDocument/types.ts
@@ -1,9 +1,9 @@
-import { IAuthenticationSectionAttrs, IPublicKeySectionAttrs } from './sections/types'
+import { IPublicKeySectionAttrs, IAuthenticationSectionAttrs } from './sections/types'
 
 export interface IDidDocumentAttrs {
   '@context': string
   id: string
-  authentication: IAuthenticationSectionAttrs[]
-  publicKey: IPublicKeySectionAttrs[]
+  authentication: IAuthenticationSectionAttrs
+  publicKey: IPublicKeySectionAttrs
   created: Date
 }
