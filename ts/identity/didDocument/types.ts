5f44617ca9a9aa9ee8bc6ab8456b1aff987ecb4d
charly <ch4rly@riseup.net>
2018-06-20 17:46:52 +0200

    updated types of DidDocument

--- ts/identity/didDocument/types.ts
+++ ts/identity/didDocument/types.ts
@@ -1,9 +1,10 @@
-import { IPublicKeySectionAttrs, IAuthenticationSectionAttrs } from './sections/types'
+import { IPublicKeySectionAttrs, IAuthenticationSectionAttrs, IServiceEndpointSectionAttrs  } from './sections/types'
 
 export interface IDidDocumentAttrs {
   '@context': string
   id: string
   authentication: IAuthenticationSectionAttrs
   publicKey: IPublicKeySectionAttrs
+  serviceEndpoints: IServiceEndpointSectionAttrs
   created: Date
 }
