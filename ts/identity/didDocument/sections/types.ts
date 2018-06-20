a7834150de820aa0357d1ac939281a9b60329919
charly <ch4rly@riseup.net>
2018-06-20 17:45:32 +0200

    added interface for service endpoints section

--- ts/identity/didDocument/sections/types.ts
+++ ts/identity/didDocument/sections/types.ts
@@ -15,3 +15,10 @@ export interface IServiceEndpointSectionAttrs {
   serviceEndpoint: string
   description: string
 }
+
+export interface IServiceEndpointSectionAttrs {
+  id: string
+  'type': string[]
+  serviceEndpoint: string
+  description: string
+}
