db0ff2ef45ac8389df94bf9fdf59c0d74e234c71
Kasia Szawan <kat.szwn@gmail.com>
2018-06-21 13:02:38 +0200

    Renamed didDocument to identity, added getters and setters to identity

--- ts/identityWallet/identityWallet.ts
+++ ts/identityWallet/identityWallet.ts
@@ -2,13 +2,21 @@ import { DidDocument } from '../identity/didDocument';
 
 export class IdentityWallet {
   private privateIdentityKey: Buffer
-  private DidDocument: DidDocument
+  // TODO: change to be an instance of Identity class
+  private identity: DidDocument
 
   public static create(privateIdentityKey: Buffer): IdentityWallet {
     const identityWallet = new IdentityWallet()
     identityWallet.privateIdentityKey = privateIdentityKey
-
     return identityWallet
   }
-  
+
+  // TODO: change to be an instance of Identity class
+  public getIdentity(): DidDocument {
+    return this.identity
+  }
+
+  public setIdentity(identity: DidDocument): void {
+    this.identity = identity
+  }
 }
