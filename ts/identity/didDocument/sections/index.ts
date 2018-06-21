bc6edd98024f8c291f2240a3066177bcfeb5c90e
charly <ch4rly@riseup.net>
2018-06-21 12:25:41 +0200

    added serviceEndpointsSection to DDO

--- ts/identity/didDocument/sections/index.ts
+++ ts/identity/didDocument/sections/index.ts
@@ -1,2 +1,3 @@
 import { PublicKeySection } from './publicKeySection'
 import { AuthenticationSection } from './authenticationSection'
+import { ServiceEndpointsSection} from './serviceEndpointsSection'
