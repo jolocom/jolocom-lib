3d23528b395a1c04aae69adfa1a5135d624f85e4
Kasia Szawan <kat.szwn@gmail.com>
2018-06-22 12:43:41 +0200

    Remove credentials module from index

--- ts/index.ts
+++ ts/index.ts
@@ -1,23 +1,17 @@
 import 'reflect-metadata'
-import { Identity } from './identity'
 import { IdentityWallet } from './wallet'
-import { Credentials } from './credentials'
 import { ILibConfig } from './types'
 import { IDefaultClaimsMetadata } from './credentials/credential/types'
 import { defaultConfig } from './defaultConfig'
 import { SSO } from './sso'
 
 export class JolocomLib {
-  public identity: Identity
   public wallet: IdentityWallet
-  public credentials: Credentials
   public sso: SSO
 
   // TODO Creation process should be changed.
   constructor(config: ILibConfig = defaultConfig) {
     this.wallet = new IdentityWallet()
-    this.identity = new Identity(config)
-    this.credentials = new Credentials()
     this.sso = new SSO()
   }
 }
