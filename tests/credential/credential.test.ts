bde5a5b9b7d9edb026bde738ab928e06e8f280f6
Kasia Szawan <kat.szwn@gmail.com>
2018-06-22 12:41:27 +0200

    Fix credentials test

--- tests/credential/credential.test.ts
+++ tests/credential/credential.test.ts
@@ -1,5 +1,5 @@
 import { expect } from 'chai'
-import { Credential } from '../../ts/credentials/credential/index'
+import { Credential } from '../../ts/credentials/credential/credential'
 import { claimsMetadata } from '../../ts/index'
 import {
   singleClaimCreationArgs,
@@ -14,32 +14,32 @@ import {
 describe('Credential', () => {
   describe('static create method', () => {
     it('Should correctly assemble a credential given one claim', () => {
-      const credential = Credential.create(claimsMetadata.emailAddress, singleClaimCreationArgs)
+      const credential = Credential.create({metadata: claimsMetadata.emailAddress, claim: singleClaimCreationArgs})
       const credentialFromJSON = Credential.fromJSON(singleClaimCredentialJSON)
       expect(credential).to.deep.equal(credentialFromJSON)
     })
 
     it('Should correctly assemble a credential given multiple claims', () => {
-      const credential = Credential.create(claimsMetadata.name, multipleClaimsCreationArgs)
+      const credential = Credential.create({metadata: claimsMetadata.name, claim: multipleClaimsCreationArgs})
       const credentialFromJSON = Credential.fromJSON(multipleClaimsCredentialJSON)
       expect(credential).to.deep.equal(credentialFromJSON)
     })
 
     it('Should correctly assemble a custom credential', () => {
-      const customCredential = Credential.create(customClaimMetadata, customCredentialCreationArgs)
+      const customCredential = Credential.create({metadata: customClaimMetadata, claim: customCredentialCreationArgs})
       const customCredentialFromJSON = Credential.fromJSON(customCredentialJSON)
       expect(customCredential).to.deep.equal(customCredentialFromJSON)
     })
 
     it('Should throw in case not all claims are provided', () => {
-      expect(() => Credential.create(claimsMetadata.emailAddress, { id: 'did:jolo:test' })).to.throw(
+      expect(() => Credential.create({metadata: claimsMetadata.emailAddress, claim: { id: 'did:jolo:test' }})).to.throw(
         'Missing claims, expected keys are: email'
       )
     })
   })
 
   it('Should implement all getter methods', () => {
-    const credential = Credential.create(claimsMetadata.emailAddress, singleClaimCreationArgs)
+    const credential = Credential.create({metadata: claimsMetadata.emailAddress, claim: singleClaimCreationArgs})
 
     expect(credential.getClaim()).to.deep.equal(singleClaimCreationArgs)
     expect(credential.getContext()).to.deep.equal(claimsMetadata.emailAddress.context)
@@ -48,20 +48,20 @@ describe('Credential', () => {
   })
 
   it('Should implement static fromJSON method', () => {
-    const credential = Credential.create(claimsMetadata.emailAddress, singleClaimCreationArgs)
+    const credential = Credential.create({metadata: claimsMetadata.emailAddress, claim: singleClaimCreationArgs})
     const credFromJson = Credential.fromJSON(singleClaimCredentialJSON)
     expect(credential).to.deep.equal(credFromJson)
 
-    const nameCredential = Credential.create(claimsMetadata.name, multipleClaimsCreationArgs)
+    const nameCredential = Credential.create({metadata: claimsMetadata.name, claim: multipleClaimsCreationArgs})
     const nameCredentialFromJSON = Credential.fromJSON(multipleClaimsCredentialJSON)
     expect(nameCredential).to.deep.equal(nameCredentialFromJSON)
   })
 
   it('Should implement toJSON method', () => {
-    const credential = Credential.create(claimsMetadata.emailAddress, singleClaimCreationArgs)
+    const credential = Credential.create({metadata: claimsMetadata.emailAddress, claim: singleClaimCreationArgs})
     expect(credential.toJSON()).to.deep.equal(singleClaimCredentialJSON)
 
-    const nameCredential = Credential.create(claimsMetadata.name, multipleClaimsCreationArgs)
+    const nameCredential = Credential.create({metadata: claimsMetadata.name, claim: multipleClaimsCreationArgs})
     expect(nameCredential.toJSON()).to.deep.equal(multipleClaimsCredentialJSON)
   })
 })
