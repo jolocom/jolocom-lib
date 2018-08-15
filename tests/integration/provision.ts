b32fb7a676fece90dbe93a167694d0abc05607f1
Kasia Szawan <kat.szwn@gmail.com>
2018-08-15 12:15:51 +0200

    WIP Deploy a contract

--- tests/integration/provision.ts
+++ tests/integration/provision.ts
@@ -4,24 +4,46 @@ import * as registryContract from 'jolocom-registry-contract'
 import {
   testPrivateEthereumKey,
 } from '../data/keys'
+import { checkServerIdentity } from 'tls';
 const Web3 = require('web3')
 const IPFSFactory = require('ipfsd-ctl')
 
 const testAccount = wallet.fromPrivateKey(testPrivateEthereumKey).getAddress().toString('hex')
+const fromAccount = '0x3a05343912C4D59948c11567788006114eB15FF0'
 
 const server = ganache.server(
   {
-    accounts: [`${testAccount},1`]
+    accounts: [
+      `${testAccount},0.1`,
+      `${fromAccount},100`,
+      '0xD8538c2A03373C2D96535D61ecca182A3F413893,100'
+    ]
   }
 )
 const daemonFactory = IPFSFactory.create({type: 'go'})
 
-const deployContract = () => {
-  const web3 = new Web3(server)
+const deployContract = async () => {
+  const web3 = new Web3()
+  console.log(server.provider.options.accounts)
+  web3.setProvider(server.provider)
+  web3.eth.setProvider(server.provider)
+  web3.eth.personal.setProvider(server.provider)
+  const accounts = await web3.eth.getAccounts()
+  const pass = '@#%*&SecurePass'
+  const account = await web3.eth.personal.newAccount(pass)
+  await web3.eth.personal.unlockAccount(account, pass, 10000)
+  console.log(accounts)
+  console.log(account)
+  const address = await registryContract.TestDeployment.deployIdentityContract(
+    web3,
+    '0xD8538c2A03373C2D96535D61ecca182A3F413893'
+  )
+  console.log(address)
+  return address
 }
 
 export const init = async () => {
-  return new Promise((resolve, reject) => {
+  return new Promise(async (resolve, reject) => {
     server.listen(8546, (ganacheErr, blockchain) => {
         if (ganacheErr) { return reject(ganacheErr) }
 
@@ -40,11 +62,12 @@ export const init = async () => {
               if (apiErr) {
                 return reject(apiErr);
               }
-              return resolve()
+              resolve()
             });
           }
         )
       })
+    return await deployContract()
   })
 }
 // const startTest = () => {
