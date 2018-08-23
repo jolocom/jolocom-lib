d1684bac3f6908c8882f2cff8091791e099e4cdb
Exulansis <rusu.eugeniu@gmx.de>
2018-08-23 14:30:30 +0200

    Finalized test fix

--- tests/integration/index.ts
+++ tests/integration/index.ts
@@ -2,7 +2,6 @@ import * as chai from 'chai'
 import * as sinonChai from 'sinon-chai'
 import {
   testPrivateIdentityKey,
-  testPublicIdentityKey,
   testPrivateEthereumKey
 } from '../data/keys'
 import { DidDocument } from '../../ts/identity/didDocument'
@@ -16,6 +15,7 @@ import { EthResolver } from '../../ts/ethereum'
 import { setTimeout } from 'timers'
 import { IdentityWallet } from '../../ts/identityWallet/identityWallet'
 import { ddoAttr } from '../data/identity'
+import { createJolocomRegistry } from '../../ts/registries/jolocomRegistry';
 
 chai.use(sinonChai)
 const expect = chai.expect
@@ -40,57 +40,54 @@ describe('IdentityWallet', () => {
 
     const ipfsConnector = new IpfsStorageAgent(ipfsConfig)
     const ethereumConnector = new EthResolver(ethereumConfig)
-    jolocomRegistry = registries.jolocom.create({ ipfsConnector, ethereumConnector })
-    const ddo = await new DidDocument().fromPrivateKey(testPublicIdentityKey)
+    jolocomRegistry = createJolocomRegistry({ ipfsConnector, ethereumConnector })
+    const ddo = await new DidDocument().fromPrivateKey(testPrivateIdentityKey)
     const identity = Identity.create({ didDocument: ddo.toJSON() })
   })
 
-  it('true', () => {
-    expect(true).to.be.true
+  describe('Creation of identity', () => {
+
+    it('should generate a valid DDO', async () => {
+      const identityWallet: IdentityWallet = await jolocomRegistry.create({
+        privateIdentityKey: testPrivateIdentityKey,
+        privateEthereumKey: testPrivateEthereumKey
+      })
+
+      const didDocument = identityWallet.getIdentity().didDocument
+
+      expect(didDocument).to.be.an.instanceOf(DidDocument)
+      expect(didDocument.getDID()).to.eq('did:jolo:5dcbd50085819b40b93efc4f13fb002119534e9374274b10edce88df8cb311af')
+    })
+
   })
 
-  // describe('Creation of identity', () => {
-  //   it('should generate a valid DDO', async () => {
-  //     const identityWallet: IdentityWallet = await jolocomRegistry.create({
-  //       privateIdentityKey: testPrivateIdentityKey,
-  //       privateEthereumKey: testPrivateEthereumKey
-  //     })
+  // errors out now; publicProfile changes will fix it
+  // describe('Authentication', () => {
 
-  //     const didDocument = identityWallet.getIdentity().didDocument
+  //   it('should return authenticated identity wallet', async () => {
+  //     const authenticated = await jolocomRegistry.authenticate(testPrivateIdentityKey)
 
-  //     expect(didDocument).to.be.an.instanceOf(DidDocument)
-  //     expect(didDocument.getDID()).to.eq('did:jolo:5dcbd50085819b40b93efc4f13fb002119534e9374274b10edce88df8cb311af')
+  //     expect(authenticated).to.be.an.instanceOf(IdentityWallet)
+  //     expect(authenticated.getIdentity().getDID())
+  //       .to.eq('did:jolo:5dcbd50085819b40b93efc4f13fb002119534e9374274b10edce88df8cb311af')
   //   })
 
   // })
 
-  // // errors out now; publicProfile changes will fix it
-  // // describe('Authentication', () => {
+  describe('Signature verification', () => {
 
-  // //   it('should return authenticated identity wallet', async () => {
-  // //     const authenticated = await jolocomRegistry.authenticate(testPrivateIdentityKey)
+    it('should generate a valid DDO', async () => {
+     return true
+    })
 
-  // //     expect(authenticated).to.be.an.instanceOf(IdentityWallet)
-  // //     expect(authenticated.getIdentity().getDID())
-  // //       .to.eq('did:jolo:5dcbd50085819b40b93efc4f13fb002119534e9374274b10edce88df8cb311af')
-  // //   })
+  })
 
-  // // })
+  describe('Signed credentials', () => {
 
-  // describe('Signature verification', () => {
+    it('should generate a valid DDO', async () => {
+      return true
+    })
 
-  //   it('should generate a valid DDO', async () => {
-  //    return true
-  //   })
-
-  // })
-
-  // describe('Signed credentials', () => {
-
-  //   it('should generate a valid DDO', async () => {
-  //     return true
-  //   })
-
-  // })
+  })
 
 })
--- ts/registries/index.ts
+++ ts/registries/index.ts
@@ -1,6 +1,5 @@
 import { createJolocomRegistry } from './jolocomRegistry'
 
-console.log(createJolocomRegistry)
 export const registries = {
   jolocom: {
     create: createJolocomRegistry
