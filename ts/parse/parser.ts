5d8da4c1010e1d5a888e8557c118f3d10e28e2cf
Kasia Szawan <kat.szwn@gmail.com>
2018-09-07 14:59:44 +0200

    Include CredentialRequest in parse module

--- ts/parse/parser.ts
+++ ts/parse/parser.ts
@@ -1,9 +1,11 @@
 import { CredentialParser } from '../credentials/credential/credentialParser'
 import { SignedCredentialParser } from '../credentials/signedCredential/signedCredentialParser';
 import { JSONWebToken } from '../interactionFlows/jsonWebToken';
+import { CredentialRequestParser } from '../interactionFlows/credentialRequest/credentialRequestParser';
 
 export const parse = {
   interactionJSONWebToken: JSONWebToken,
   credential: CredentialParser,
+  credentialRequest : CredentialRequestParser,
   signedCredential : SignedCredentialParser
 }
