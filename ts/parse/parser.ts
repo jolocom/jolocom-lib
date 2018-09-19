1b38227c0b79cfe0d09af07dea65b2a0bafeec23
natascha <natascha@nataschas-MacBook-Pro.local>
2018-09-19 17:34:15 +0200

    parser added

--- ts/parse/parser.ts
+++ ts/parse/parser.ts
@@ -1,13 +1,15 @@
 import { CredentialParser } from '../credentials/credential/credentialParser'
 import { SignedCredentialParser } from '../credentials/signedCredential/signedCredentialParser'
 import { CredentialRequestParser } from '../interactionFlows/credentialRequest/credentialRequestParser'
-import { CredentialResponseParser } from '../interactionFlows/credentialResponse/credentialResponseParser';
-import { JSONWebTokenParser } from '../interactionFlows/JSONWebTokenParser';
+import { CredentialResponseParser } from '../interactionFlows/credentialResponse/credentialResponseParser'
+import { JSONWebTokenParser } from '../interactionFlows/JSONWebTokenParser'
+import { CredentialReceiveParser } from '../interactionFlows/credentialReceive/credentialReceiveParser'
 
 export const parse = {
   interactionJSONWebToken: JSONWebTokenParser,
   credential: CredentialParser,
   credentialRequest : CredentialRequestParser,
   credentialResponse: CredentialResponseParser,
+  credentialReceive: CredentialReceiveParser,
   signedCredential : SignedCredentialParser
 }
