dcfa3c18011be336939edacb3823094893c2daa9
Charly <fei.charleen@gmail.com>
2018-09-19 17:46:27 +0200

    re-inserted necessary attributes into payload data

--- tests/data/interactionFlows/credentialRequest.ts
+++ tests/data/interactionFlows/credentialRequest.ts
@@ -1,6 +1,8 @@
 import { InteractionType } from '../../../ts/interactionFlows/types'
 
 export const credentialRequestPayloadJson = {
+  iss: 'did:jolo:8f977e50b7e5cbdfeb53a03c812913b72978ca35c93571f85e862862bac8cdeb',
+  iat: 0,
   typ: InteractionType.CredentialRequest.toString(),
   credentialRequest: {
     callbackURL: 'http://test.com',
