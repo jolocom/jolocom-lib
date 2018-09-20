62c5c197125ea2b89a216ab0684f4aa8825ed965
natascha <natascha@nataschas-MacBook-Pro.local>
2018-09-20 13:56:47 +0200

    authentication request classes added

---
+++ ts/interactionFlows/authenticationRequest/authenticationRequest.ts
@@ -0,0 +1,30 @@
+import { plainToClass, classToPlain } from 'class-transformer'
+import { IAuthenticationRequestAttrs } from './types'
+
+export class AuthenticationRequest {
+  private challenge: string
+  private callbackURL: string
+
+  public static create(attrs: IAuthenticationRequestAttrs): AuthenticationRequest {
+    const authReq = new AuthenticationRequest()
+    authReq.challenge = attrs.challenge
+
+    return authReq
+  }
+
+  public getChallenge(): string {
+    return this.challenge
+  }
+
+  public getCallbackURL(): string {
+    return this.callbackURL
+  }
+
+  public toJSON(): IAuthenticationRequestAttrs {
+    return classToPlain(this) as IAuthenticationRequestAttrs
+  }
+
+  public static fromJSON(json: IAuthenticationRequestAttrs): AuthenticationRequest {
+    return plainToClass(AuthenticationRequest, json)
+  }
+}
---
+++ ts/interactionFlows/authenticationRequest/authenticationRequestParser.ts
@@ -0,0 +1,5 @@
+import { AuthenticationRequest } from './authenticationRequest'
+
+export const AuthenticationRequestParser = {
+  fromJSON : AuthenticationRequest.fromJSON
+}
