99bf1e62d97532c76cf8ae76bafd6df510f6b35d
natascha <natascha@nataschas-MacBook-Pro.local>
2018-09-25 11:19:40 +0200

    missing tests added on payload classes credResponse & credRequest

--- tests/interactionFlows/credentialResponsePayload.test.ts
+++ tests/interactionFlows/credentialResponsePayload.test.ts
@@ -1,22 +1,41 @@
 import { expect } from 'chai'
-import { credentialResponsePayloadJson } from './../data/interactionFlows/credentialResponse';
+import { credentialResponsePayloadJson } from './../data/interactionFlows/credentialResponse'
 import { CredentialResponsePayload } from '../../ts/interactionFlows/credentialResponse/credentialResponsePayload'
+import { CredentialResponse } from '../../ts/interactionFlows/credentialResponse/credentialResponse'
+import { CredentialRequest } from '../../ts/interactionFlows/CredentialRequest/credentialRequest'
+import { credentialRequestCreationAttrs } from '../data/interactionFlows/credentialRequest'
 
 describe('CredentialResponsePayload', () => {
-  it('Should implement static fromJSON method which returns a valid instance of CredentialResponsePayload', () => {
-    const crp = CredentialResponsePayload.fromJSON(credentialResponsePayloadJson)
+  const crp = CredentialResponsePayload.fromJSON(credentialResponsePayloadJson)
+
+  it('Should correctly return a credentialResponsePayload class on static create method', () => {
+    const credResPayload = CredentialResponsePayload.create(credentialResponsePayloadJson)
+    credResPayload.iss = credentialResponsePayloadJson.iss
+    credResPayload.iat = credentialResponsePayloadJson.iat
+
+    expect(credResPayload).to.deep.equal(crp)
+    expect(credResPayload).to.be.instanceOf(CredentialResponsePayload)
+    expect(credResPayload.credentialResponse).to.be.instanceOf(CredentialResponse)
+    expect(credResPayload.getSuppliedCredentials()).to.deep.equal(crp.getSuppliedCredentials())
+  })
+
+  it('Should return true on credentialResponse.satisfiesRequest call', () => {
+    const cr = CredentialRequest.create(credentialRequestCreationAttrs)
+  
+    expect(crp.credentialResponse.satisfiesRequest(cr)).to.be.true
+  })
+
+  it('Should implement static fromJSON method which returns a valid instance of CredentialResponsePayload', () => {  
     expect(crp.credentialResponse.getSuppliedCredentials())
       .to.deep.equal(credentialResponsePayloadJson.credentialResponse.suppliedCredentials)
   })
 
   it('Should implement toJSON method which returns a correct JSON', () => {
-    const crp = CredentialResponsePayload.fromJSON(credentialResponsePayloadJson)
     const json = crp.toJSON()
     expect(json).to.deep.equal(credentialResponsePayloadJson)
   })
 
   it('Should expose CredentialResponse specific methods', () => {
-    const crp = CredentialResponsePayload.fromJSON(credentialResponsePayloadJson)
     expect(crp.getSuppliedCredentials).to.exist
   })
 })
