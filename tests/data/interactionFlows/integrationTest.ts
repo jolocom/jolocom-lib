557eb3ee728acc03a12d5fc3003c266aabd991a8
nataschaberg <natalja.wittenberg@gmx.de>
2018-09-29 01:05:01 +0200

    clean up test data

--- tests/data/interactionFlows/integrationTest.ts
+++ tests/data/interactionFlows/integrationTest.ts
@@ -65,8 +65,8 @@ export const sampleCredentialRequest = {
   callbackURL: 'http://test.com',
   credentialRequirements: [
     {
-      type: ['Credential', 'MockCredential'],
-      constraints: [{ '==': [{ var: 'issuer' }, 'did:jolo:issuer'] }]
+      type: [ 'Credential', 'ProofOfEmailCredential' ],
+      constraints: [{ '==': [{ var: 'issuer' }, 'did:jolo:5dcbd50085819b40b93efc4f13fb002119534e9374274b10edce88df8cb311af'] }]
     }
   ]
 }
