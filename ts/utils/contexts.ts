849da46669946b8db31e58b05e12d2a4a0510f22
nataschaberg <natalja.wittenberg@gmx.de>
2018-10-11 20:21:06 +0200

    default context adjusted

--- ts/utils/contexts.ts
+++ ts/utils/contexts.ts
@@ -1,8 +1,26 @@
 import { ContextEntry } from 'cred-types-jolocom-core'
 
 export const defaultContext: ContextEntry[] = [
-  'https://w3id.org/identity/v1',
   {
-    proof: 'https://w3id.org/security#proof'
+    id: '@id',
+    type: '@type',
+    cred: 'https://w3id.org/credentials#',
+    schema: 'http://schema.org/',
+    dc: 'http://purl.org/dc/terms/',
+    xsd: 'http://www.w3.org/2001/XMLSchema#',
+    sec: 'https://w3id.org/security#',
+    Credential: 'cred:Credential',
+    issuer: { '@id': 'cred:issuer', '@type': '@id' },
+    issued: { '@id': 'cred:issued', '@type': 'xsd:dateTime' },
+    claim: { '@id': 'cred:claim', '@type': '@id' },
+    credential: { '@id': 'cred:credential', '@type': '@id' },
+    expires: { '@id': 'sec:expiration', '@type': 'xsd:dateTime' },
+    proof: { '@id': 'sec:proof', '@type': '@id' },
+    EcdsaKoblitzSignature2016: 'sec:EcdsaKoblitzSignature2016',
+    created: { '@id': 'dc:created', '@type': 'xsd:dateTime' },
+    creator: { '@id': 'dc:creator', '@type': '@id' },
+    domain: 'sec:domain',
+    nonce: 'sec:nonce',
+    signatureValue: 'sec:signatureValue'
   }
 ]
