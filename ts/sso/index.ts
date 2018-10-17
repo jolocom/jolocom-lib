a2011deb56813001f27ff0f5549556ca67746073
Kasia Szawan <kat.szwn@gmail.com>
2018-10-17 13:41:24 +0200

    Add options param to JWTtoQR and pass those to the qr code generation method

--- ts/sso/index.ts
+++ ts/sso/index.ts
@@ -1,7 +1,9 @@
 import * as QRCode from 'qrcode'
 
+/* For options, please refer to qrcode npm package documentation
+ * https://github.com/soldair/node-qrcode **/
 export class SSO {
-  public async JWTtoQR(jwt: string): Promise<string> {
-    return await QRCode.toDataURL(jwt)
+  public async JWTtoQR(jwt: string, options?: object): Promise<string> {
+    return await QRCode.toDataURL(jwt, options)
   }
 }
