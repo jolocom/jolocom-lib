8fec3de54d62bcb887664663ff3fd4f67ddf229e
Exulansis <rusu.eugeniu@gmx.de>
2018-11-11 17:48:57 +0100

    Api documentation for ethereum connector

--- ts/ethereum/ethereum.ts
+++ ts/ethereum/ethereum.ts
@@ -4,6 +4,7 @@ import { IEthereumResolverConfig, IEthereumConnector, IEthereumResolverUpdateDID
 /**
  * @class
  * Class abstracting all interactions with the registry smart contract
+ * @internal
  */
 
 export class EthResolver implements IEthereumConnector {
@@ -12,9 +13,6 @@ export class EthResolver implements IEthereumConnector {
   /**
    * @constructor
    * @param config - Configuration to connect to a remote Ethereum node, and the address of the registry contract
-   * @param config.providerUrl - Endpoint to connect to an Ethereum http api gateway, e.g. infura
-   * @param config.contractAddress - The address of the Jolocom registry smart contract
-   * @returns {EthereumResolver} - Instance of configured registry
    */
 
   constructor(config: IEthereumResolverConfig) {
@@ -22,23 +20,23 @@ export class EthResolver implements IEthereumConnector {
   }
 
   /**
-   * @todo - This should throw an error, or return false in case no record is found
-   * @description - Returns a reference to the did document given the did or an empty string
+   * Returns a reference to the did document given the did or an empty string
    * @param did - Did to resolve
-   * @returns {Promise<string>} - IPFS hash of the did document
+   * @example `console.log(await resolver.resolveDid('did:jolo:...')) // 'QmZCEmf...'`
    */
 
+  /*  @TODO - This should throw an error, or return false in case no record is found */
   public async resolveDID(did: string): Promise<string> {
     return this.ethResolver.resolveDID(did)
   }
 
   /**
-   * @description - Updates the mapping in the smart contract to reference a new pointer
+   * Updates the mapping in the smart contract to reference a new pointer
    * @deprecated - Will be deprecated in next major release to avoid relying on private keys in the codebase
    * @param ethereumKey - Ethereum private key to sign the transaction
    * @param did - Did to update
    * @param newHash - New IPFS hash to associate with the did
-   * @returns {Promise<void>}
+   * @example `await resolver.updateDIDRecord({ethereumKey: Buffer.from('...'), did: 'did:jolo:...', 'QmZCEmf...'})`
    */
 
   public async updateDIDRecord({ ethereumKey, did, newHash }: IEthereumResolverUpdateDIDArgs): Promise<void> {
