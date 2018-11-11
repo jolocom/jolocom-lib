119ba2f91285cb39903d6acbdb60c230bc26f3b4
Exulansis <rusu.eugeniu@gmx.de>
2018-11-11 22:12:32 +0100

    Documentation on ipfs agent and ecdsaKoblitzSignature classess.

--- ts/linkedDataSignature/suites/ecdsaKoblitzSignature2016.ts
+++ ts/linkedDataSignature/suites/ecdsaKoblitzSignature2016.ts
@@ -5,6 +5,13 @@ import { ILinkedDataSignature, ILinkedDataSignatureAttrs, IDigestable } from '..
 import { sha256 } from '../../utils/crypto'
 import { defaultContext } from '../../utils/contexts'
 
+/**
+ * @class A EcdsaKoblitz linked data signature implementation
+ * @implements {ILinkedDataSignature}
+ * @implements {IDigestable}
+ * @internal
+ */
+
 @Exclude()
 export class EcdsaLinkedDataSignature implements ILinkedDataSignature, IDigestable {
   private _type = 'EcdsaKoblitzSignature2016'
@@ -13,6 +20,11 @@ export class EcdsaLinkedDataSignature implements ILinkedDataSignature, IDigestab
   private _nonce: string = ''
   private _signatureValue: string = ''
 
+  /**
+   * Get the creation date of the linked data signature
+   * @example `console.log(proof.created) // Date 2018-11-11T15:46:09.720Z`
+   */
+
   @Expose()
   @Type(() => Date)
   @Transform((value: Date) => value && value.toISOString(), { toPlainOnly: true })
@@ -20,51 +32,95 @@ export class EcdsaLinkedDataSignature implements ILinkedDataSignature, IDigestab
     return this._created
   }
 
+  /**
+   * Set the creation date of the linked data signature
+   * @example `proof.created = Date 2018-11-11T15:46:09.720Z`
+   */
+
   set created(created: Date) {
     this._created = created
   }
+
+  /**
+   * Get the type of the linked data signature
+   * @example `console.log(proof.type) // 'EcdsaKoblitzSignature2016'`
+   */
 
   @Expose()
   get type() {
     return this._type
   }
 
+  /**
+   * Set the type of the linked data signature
+   * @example `proof.type = 'EcdsaKoblitzSignature2016'`
+   */
+
   set type(type: string) {
     this._type = type
   }
+
+  /**
+   * Get the random signature nonce
+   * @example `console.log(proof.nonce) // 'abc...fe'`
+   */
 
   @Expose()
   get nonce() {
     return this._nonce
   }
 
+  /**
+   * Set the random signature nonce
+   * @example `proof.nonce = 'abc...fe'`
+   */
+
   set nonce(nonce: string) {
     this._nonce = nonce
   }
 
-  @Expose({name: 'signatureValue'})
+  /**
+   * Get the hex encoded signature value
+   * @example `console.log(proof.signature) // '2b8504698e...'`
+   */
+
+  @Expose({ name: 'signatureValue' })
   get signature() {
     return this._signatureValue
   }
 
+  /**
+   * Set the hex encoded signature value
+   * @example `proof.signature = '2b8504698e...'`
+   */
+
   set signature(signature: string) {
     this._signatureValue = signature
   }
+
+  /**
+   * Get the identifier of the public signing key
+   * @example `console.log(proof.creator) // 'did:jolo:...#keys-1`
+   */
 
   @Expose()
   get creator(): string {
     return this._creator
   }
 
+  /**
+   * Set the identifier of the public signing key
+   * @example `proof.creator = 'did:jolo:...#keys-1`
+   */
+
   set creator(creator: string) {
     this._creator = creator
   }
 
-  /*
-   * @description - Converts JSON-LD signature to canonical form
-   *  see https://w3c-dvcg.github.io/ld-signatures/#signature-algorithm
-   * @returns {Object} - Document in normalized form, quads
-  */
+  /**
+   * Converts the lined data signature to canonical form
+   * @see {@link https://w3c-dvcg.github.io/ld-signatures/#dfn-canonicalization-algorithm | Canonicalization algorithm }
+   */
 
   private async normalize(): Promise<string> {
     const json: ILinkedDataSignatureAttrs = this.toJSON()
@@ -75,19 +131,27 @@ export class EcdsaLinkedDataSignature implements ILinkedDataSignature, IDigestab
     return canonize(json)
   }
 
-  /*
-   * @description - Computes a 256 bit digest that can be signed later
-   * @returns {Buffer} - 32 byte sha256 digest of normalized JSON-LD signature
-  */
+  /**
+   * Returns the sha256 hash of the linked data signature, per {@link https://w3c-dvcg.github.io/ld-signatures/#signature-algorithm | specification}.
+   */
 
   public async digest(): Promise<Buffer> {
     const normalized = await this.normalize()
     return sha256(Buffer.from(normalized))
   }
 
+  /**
+   * Instantiates a {@link EcdsaLinkedDataSignature} from it's JSON form
+   * @param json - Linked data signature in JSON-LD form
+   */
+
   public static fromJSON(json: ILinkedDataSignatureAttrs): EcdsaLinkedDataSignature {
     return plainToClass(EcdsaLinkedDataSignature, json)
   }
+
+  /**
+   * Serializes the {@link EcdsaLinkedDataSignature} as JSON-LD
+   */
 
   public toJSON(): ILinkedDataSignatureAttrs {
     return classToPlain(this) as ILinkedDataSignatureAttrs
