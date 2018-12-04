694c9f9b1b7458adb478de81e4fdd694f0d802c9
Exulansis <rusu.eugeniu@gmx.de>
2018-12-04 12:23:39 +0100

    Updated TYPEDOC comment

--- ts/utils/validation.ts
+++ ts/utils/validation.ts
@@ -7,9 +7,9 @@ import { IRegistry } from '../registries/types'
  * Validates the signature on a {@link SignedCredential} or {@link JSONWebToken}
  * @param toValidate - Instance of object implementing the {@link IDigestable} interface
  * @param customRegistry - Custom registry implementation. If null, the {@link JolocomRegistry} is used
- * @returns True if signature is valid, false otherwise
  * @example `await validateDigestable(signedCredential) // true`
  * @example `await validateDigestable(signedCredential, customRegistry) // true`
+ * @returns {boolean} - True if signature is valid, false otherwise
  */
 
 export const validateDigestable = async (
@@ -30,12 +30,12 @@ export const validateDigestable = async (
 }
 
 /**
- * Validates the signatures on an array of {@link SignedCredential}s or {@link JSONWebToken}s
+ * Validates the signatures on an array of {@link SignedCredential}s or {@link JSONWebToken}
  * @param toValidate - Array of objects implementing the {@link IDigestable} interface
  * @param customRegistry - Custom registry implementation. If null, the {@link JolocomRegistry} is used
- * @returns Array<boolean> where true if signature is valid, false otherwise
  * @example `await validateDigestable(signedCredentials) // [true, false...]`
  * @example `await validateDigestable(signedCredentials, customRegistry) // [true, false...]`
+ * @returns {Array<boolean>} - Where true if signature is valid, false otherwise
  */
 
 export const validateDigestables = async (
