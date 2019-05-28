88a5bec28604fa01407b1ca076d502e7a11489de
Mina Nagy Zaki <mnzaki@gmail.com>
2019-05-28 15:42:56 +0200

    Add specVersion and migration code
    migration code is implemented using the Transform decorator from class-transformer

--- ts/identity/didDocument/sections/index.ts
+++ ts/identity/didDocument/sections/index.ts
@@ -1,2 +1,4 @@
-export { PublicKeySection } from './publicKeySection'
+import { PublicKeySection } from './publicKeySection'
+export { PublicKeySection }
 export { ServiceEndpointsSection } from './serviceEndpointsSection'
+export type AuthenticationSection = PublicKeySection | string
