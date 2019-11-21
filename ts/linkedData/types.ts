5560e21053104dbfc6526340a8537760c170660f
Charles Cunningham <c.a.cunningham6@gmail.com>
2019-11-21 17:22:10 +0100

    feat(jsonld): added typings for jsonld and contexts

---
+++ ts/linkedData/types.ts
@@ -0,0 +1,17 @@
+
+
+type JsonLdPrimitive = string | number | boolean | JsonLdObject | JsonLdObject[]
+
+export type JsonLdContextEntry = string | JsonLdObject
+
+export type JsonLdContext = JsonLdContextEntry | JsonLdContextEntry[]
+
+export interface JsonLdObject {
+  '@context'?: JsonLdContext
+  [key: string]: JsonLdPrimitive | JsonLdPrimitive[]
+}
+
+export interface SignedJsonLdObject extends JsonLdObject {
+  '@context': JsonLdContext
+  proof: ILinkedDataSignatureAttrs
+}
