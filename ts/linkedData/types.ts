4b706f63b1258e54c1b955c332445db7f9f5e5b8
Charles Cunningham <c.a.cunningham6@gmail.com>
2019-11-25 11:06:14 +0100

    refactor(contexts): change context type to be compatible with cred-types-jolocom-core

--- ts/linkedData/types.ts
+++ ts/linkedData/types.ts
@@ -1,8 +1,9 @@
 import { ILinkedDataSignatureAttrs } from '../linkedDataSignature/types'
+import { ContextEntry } from 'cred-types-jolocom-core'
 
 type JsonLdPrimitive = string | number | boolean | JsonLdObject | JsonLdObject[]
 
-export type JsonLdContextEntry = string | JsonLdObject
+export type JsonLdContextEntry = ContextEntry
 
 export type JsonLdContext = JsonLdContextEntry | JsonLdContextEntry[]
 
