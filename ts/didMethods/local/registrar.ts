6a8221b4d73d96d7a9081557d0a549af13e4469f
Charles Cunningham <c.a.cunningham6@gmail.com>
2020-08-08 17:53:57 +0200

    feat(local): use vkp for local registrar

--- ts/didMethods/local/registrar.ts
+++ ts/didMethods/local/registrar.ts
@@ -1,24 +1,32 @@
-import { Identity } from "../../identity/identity"
+import { Identity } from '../../identity/identity'
 import { getRegistrar } from 'local-did-resolver'
-import { DidDocument } from "../../identity/didDocument/didDocument"
-import { SignedCredential } from "../../credentials/signedCredential/signedCredential"
-import { IRegistrar } from "../types"
-import { createDb } from "local-did-resolver/js/db"
-import { getIdFromEvent, validateEvents, getIcp } from "@jolocom/native-utils-node"
-import { SoftwareKeyProvider } from "@jolocom/vaulted-key-provider"
+import { DidDocument } from '../../identity/didDocument/didDocument'
+import { SignedCredential } from '../../credentials/signedCredential/signedCredential'
+import { IRegistrar } from '../types'
+import { createDb } from 'local-did-resolver/js/db'
+import {
+  getIdFromEvent,
+  validateEvents,
+  getIcp,
+} from '@jolocom/native-utils-node'
+import { SoftwareKeyProvider } from '@jolocom/vaulted-key-provider'
+
+type CreationReturn = {
+  id: string
+  encryptedWallet: string
+  inceptionEvent: string
+}
 
 export class LocalRegistrar implements IRegistrar {
   public prefix = 'jolo'
   private registrar: ReturnType<typeof getRegistrar>
 
-  constructor(
-    db = createDb()
-  ) {
+  constructor(db = createDb()) {
     this.registrar = getRegistrar({
       dbInstance: db,
       create: getIcp,
       getIdFromEvent: getIdFromEvent,
-      validateEvents: validateEvents
+      validateEvents: validateEvents,
     })
   }
 
@@ -26,29 +34,42 @@ export class LocalRegistrar implements IRegistrar {
   // otherwise there's no way to update the registrar with the ICP created from
   // this.registrar.create(), and no way to resolve ourselves
   async create(keyProvider: SoftwareKeyProvider, password: string) {
-  /*
-   * potential eventual API:
-   * const icp = await this.registrar.create()
-   * this.db.append(icp)
-   * return this.registrar.valdateEvents(icp)
-   */
+    /*
+     * potential eventual API:
+     * const icp = await this.registrar.create()
+     * this.db.append(icp)
+     * return this.registrar.valdateEvents(icp)
+     */
 
     // TODO currently contains keys as well.
-    const { event } = await this.registrar.create()
+    const ret = JSON.parse(
+      await this.registrar.create(
+        keyProvider.encryptedWallet,
+        keyProvider.id,
+        password,
+      ),
+    ) as CreationReturn
+
+    keyProvider._encryptedWallet = Buffer.from(ret.encryptedWallet, 'base64')
 
     const identity = Identity.fromDidDocument({
       didDocument: DidDocument.fromJSON(
-        JSON.parse(await validateEvents(JSON.stringify[event])) // TODO A better way? Perhaps didMethod.resolvelve(ourDid), but no resolver ref here :(
-      )
+        JSON.parse(await validateEvents(JSON.stringify[ret.inceptionEvent])), // TODO A better way? Perhaps didMethod.resolvelve(ourDid), but no resolver ref here :(
+      ),
     })
 
-    this.encounter(event)
+    this.encounter(ret.inceptionEvent)
     return identity
   }
 
   // TODO Verify signature on the public profile? Or just assume it's correct
   // TODO Public profile should perhaps be JSON / any, so that the registrars can be used without having to typecheck / guard / use generics
-  async updatePublicProfile(keyProvider: SoftwareKeyProvider, password: string, identity: Identity, publicProfile: SignedCredential) {
+  async updatePublicProfile(
+    keyProvider: SoftwareKeyProvider,
+    password: string,
+    identity: Identity,
+    publicProfile: SignedCredential,
+  ) {
     console.error(`"updatePublicProfile not implemented for did:${this.prefix}`) // TODO Better error
     return false
   }
@@ -61,5 +82,3 @@ export class LocalRegistrar implements IRegistrar {
     return true
   }
 }
-
-
