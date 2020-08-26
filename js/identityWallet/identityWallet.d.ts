c5584c9ab358ea31b3d04d4502a556cf0a2c91f2
mnzaki <mnzaki@gmail.com>
2020-08-26 16:17:19 +0200

    chore: bump for native

--- js/identityWallet/identityWallet.d.ts
+++ js/identityWallet/identityWallet.d.ts
@@ -36,7 +36,8 @@ export declare class IdentityWallet {
     asymEncrypt: (data: Buffer, key: Buffer, type: KeyTypes) => Promise<Buffer>;
     asymEncryptToDidKey: (data: Buffer, keyRef: string, resolver?: import("../didMethods/types").IResolver) => Promise<Buffer>;
     asymEncryptToDid: (data: Buffer, did: string, resolver?: import("../didMethods/types").IResolver) => Promise<Buffer>;
-    asymDecrypt: (data: string, pass: string) => Promise<Buffer>;
+    asymDecrypt: (data: Buffer, pass: string) => Promise<Buffer>;
+    sign: (data: Buffer, pass: string) => Promise<Buffer>;
     create: {
         credential: typeof Credential.create;
         signedCredential: <T extends BaseMetadata>({ expires, ...credentialParams }: WithExtraOptions<ISignedCredCreationArgs<T>>, pass: string) => Promise<SignedCredential>;
